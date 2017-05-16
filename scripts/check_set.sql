3e26f03ce71d60c7ab4afc586ac85a6e4d846e65
Zaaraah <iraharcega@yahoo.com.ph>
2017-05-16 09:57:58 +0800

    "Deleted check_set.sql"

--- scripts/check_set.sql
+++
@@ -1,7 +0,0 @@
-SELECT SS.set_code, count(S.survey_id)
-FROM survey_set SS
-  LEFT JOIN survey S on S.set_code = SS.set_code
-  LEFT JOIN site_answer SA on SA.survey_id = S.survey_id
-GROUP BY SS.set_code
-ORDER BY count(S.survey_id) asc
-LIMIT 1
